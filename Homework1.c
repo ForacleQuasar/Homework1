14e9e88cc2a55c3c3b4bc3fcf90f50411dc405d9
ForacleQuasar <103261549+ForacleQuasar@users.noreply.github.com>
2024-09-17 20:03:25 +0300

    Add files via upload

---
+++ Homework1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<locale.h>
+void main()
+{
+	puts("              /----| ");
+	puts("             /     |");
+	puts("            /      |");
+	puts("           /_______|");
+	puts("           | ____  |");
+	puts("           | |_|_| |");
+	puts("           | |_|_| |");
+	puts("           |_______|");
+}
